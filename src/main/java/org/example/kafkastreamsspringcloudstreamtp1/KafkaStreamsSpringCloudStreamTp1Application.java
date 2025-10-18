3b563ac33b13152c2c687d6d70f9f0a303e2e568
latifaja <latifajakir44@gmail.com>
2025-10-19 00:22:05 +0100

    Event Driven Architecture- Spring Cloud Streams - KAFKA - KAFKA Streams

---
+++ src/main/java/org/example/kafkastreamsspringcloudstreamtp1/KafkaStreamsSpringCloudStreamTp1Application.java
@@ -0,0 +1,13 @@
+package org.example.kafkastreamsspringcloudstreamtp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsSpringCloudStreamTp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamsSpringCloudStreamTp1Application.class, args);
+    }
+
+}
